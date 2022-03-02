11197fd40aca8d47f3a4d3417f0e829e3ff436ac
TonyHuangDev <tonyhuang.dev@gmail.com>
2022-03-02 14:29:46 -0300

    Primeiro commit

---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+
+//Base URL > https://sujeitoprogramador.com/
+
+// https://sujeitoprogramador.com/r-api/?api=filmes/
+
+const api = axios.create({
+    baseURL: 'https://sujeitoprogramador.com'
+});
+
+export default api;
